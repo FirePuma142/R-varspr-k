5ca6d211cd921d8750179e3871d579c4936048a5
FirePuma142 <mans.ekenberg@icloud.com>
2024-06-19 21:28:25 +0200

    First run

---
+++ target/debug/Rövarspråk.d
@@ -0,0 +1,1 @@
+/Users/mansfalk/Code/Rust/R-varspr-k/target/debug/Rövarspråk: /Users/mansfalk/Code/Rust/R-varspr-k/src/main.rs
---
+++ target/debug/deps/Rövarspråk-bbc99d43c5b72b4d.d
@@ -0,0 +1,5 @@
+/Users/mansfalk/Code/Rust/R-varspr-k/target/debug/deps/Rövarspråk-bbc99d43c5b72b4d: src/main.rs
+
+/Users/mansfalk/Code/Rust/R-varspr-k/target/debug/deps/Rövarspråk-bbc99d43c5b72b4d.d: src/main.rs
+
+src/main.rs:
