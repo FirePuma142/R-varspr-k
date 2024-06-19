cbc8c2527c1756d3d81afa0cd16c9e170f40d682
FirePuma142 <mans.ekenberg@icloud.com>
2024-06-19 22:11:12 +0200

    Added working translation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    translate("lol");
 }
 fn translate(input: &str) -> &str{
-    let consonants = "bcdfghjklmnpqrstvwxyz";
+    let consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+    let mut translation = String::new(); 
+    for letter in input.chars(){
+        if consonants.contains(letter){
+            translation.push(letter);
+            translation.push_str("o");
+            translation.push(letter);
+        }
+        else{
+            translation.push(letter);
+        }
+    }
+    println!("{}", translation);
     input
 }
 fn de_translate(input: &str) ->&str{
