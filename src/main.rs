00a05d090668f16a1ea397dbea358884fa3480bd
FirePuma142 <mans.ekenberg@icloud.com>
2024-06-19 22:45:17 +0200

    Both functions are now working

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
-    translate("lol");
+    let input = "";
+    let translation = translate(input);
+    let de_translation = de_translate(input);
+    println!("Original string: '{}'\nTranslation: '{}'\nDetranslation: '{}'",input, translation, de_translation);
 }
-fn translate(input: &str) -> &str{
+fn translate(input: &str) -> String{
     let consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
     let mut translation = String::new(); 
     for letter in input.chars(){
@@ -14,9 +17,25 @@ fn translate(input: &str) -> &str{
             translation.push(letter);
         }
     }
-    println!("{}", translation);
-    input
+    translation
 }
-fn de_translate(input: &str) ->&str{
-    input
+fn de_translate(input: &str) ->String{
+    let consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+    let mut translation = String::new();
+    let mut i = 0;
+    let input_chars: Vec<char> = input.chars().collect();
+
+    while i < input_chars.len(){
+        let letter: char = input_chars[i];
+        
+        if consonants.contains(letter) && i + 2 <   input_chars.len() && input_chars[i+1] == 'o' && input_chars[i+2] == letter{
+            translation.push(letter);
+            i += 3;
+        }
+        else{
+            translation.push(letter);
+            i += 1;
+        }
+    }
+    translation
 }
